12904afa083f1f159036ff9993ea17d089a27be7
douglasdans <douglas.dans@gmail.com>
2024-03-07 08:23:11 -0300

    Projeto Hello World

---
+++ HelloWorld/app/src/main/java/org/fatec/helloworld/MainActivity.java
@@ -0,0 +1,33 @@
+package org.fatec.helloworld;
+
+import android.os.Bundle;
+import android.util.Log;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    public void onCreate(Bundle bundle) {
+        super.onCreate(bundle);
+
+        EditText edNome = findViewById(R.id.txtNome);
+        EditText edEmail = findViewById(R.id.txtEmail);
+        EditText edTelefone = findViewById(R.id.txtTelefone);
+
+        Button btnSubmit = findViewById(R.id.btnSubmit);
+
+        btnSubmit.setOnClickListener( e -> {
+            Log.i("Agenda_Contato", "Nome: " + edNome.getText());
+            Log.i("Agenda_Contato", "Email: " + edEmail.getText());
+            Log.i("Agenda_Contato", "Telefone: " + edTelefone.getText());
+        });
+    };
+}
